dad5e68cc14271dece1446c8a63ec36e009ab1f3
thangdhcn <thangvnguyen2001@gmail.com>
2023-10-09 14:50:01 +0700

    Tiep tuc

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Image} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.view1}>
+        <Text>Show</Text>
+        </View>  
+      <View style={styles.view2}>
+      <Text>Show</Text>
+      <Button></Button>
+      <Button></Button>
+      <Image source={ require('./assets/icon.png')  
+      }
+        style={{width:'100px', height:'100px'}}
+      ></Image>
+        </View>  
+      <View style={styles.view3}><Text>Show</Text></View>     
     </View>
   );
 }
@@ -13,8 +24,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundImage: 'linear-gradient(45deg, #ff0000, #0000ff)',
+   
   },
+  view1:{
+    flex: 2
+  },
+  view2:{
+    flex: 6,
+    flexDirection:'column',
+    alignItems:'center',
+    justifyContent:'space-around'
+  }, view3:{
+    flex: 2
+  }
 });
